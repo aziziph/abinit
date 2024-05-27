94160c65ab6a966e4f6fc0873ce228a193b54058
mazizi <maryam.azizi@uclouvain.be>
2024-05-27 09:28:34 +0200

    additional information

--- docs/abinit_info.js
+++ docs/abinit_info.js
@@ -16,23 +16,10 @@ document.addEventListener('DOMContentLoaded', function() {
         moreLink.style.display = 'inline';
         lessLink.style.display = 'none';
     });
-
-    // Toggle visibility of details when clicking the "-" sign
-    lessLink.addEventListener('click', function(event) {
-        event.preventDefault();
-        if (details.style.display === 'none') {
-            details.style.display = 'inline';
-            moreLink.style.display = 'none';
-            lessLink.style.display = 'inline';
-        } else {
-            details.style.display = 'none';
-            moreLink.style.display = 'inline';
-            lessLink.style.display = 'none';
-        }
-    });
 });
+
 $('#carouselExampleIndicators').on('slid.bs.carousel', function (e) {
-  var slideIndex = $(this).find('.carousel-item.active').index();
-  $(this).find('.carousel-indicators li').removeClass('active');
-  $(this).find('.carousel-indicators li').eq(slideIndex).addClass('active');
+    var slideIndex = $(this).find('.carousel-item.active').index();
+    $(this).find('.carousel-indicators li').removeClass('active');
+    $(this).find('.carousel-indicators li').eq(slideIndex).addClass('active');
 });
